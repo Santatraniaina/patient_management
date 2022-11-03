b99801231c8e1cfe227e6549cfbe32b09bd9d60a
Santatraniaina <santatraniaina21@gmail.com>
2022-11-03 09:22:20 +0300

    [FIX] Fix some bug in report and traduction

--- patient_management/wizard/patient_birthdate.py
+++ patient_management/wizard/patient_birthdate.py
@@ -1,4 +1,7 @@
-from odoo import fields, models
+from datetime import date
+
+from odoo import fields, models, api
+
 
 class PatientBirthdate(models.TransientModel):
     _name = 'patient.birthdate'
@@ -6,4 +9,11 @@ class PatientBirthdate(models.TransientModel):
 
     birthdate = fields.Date(string='Birthdate')
 
-    # def compute_age(self):
+    @api.depends('birthdate')
+    def compute_age(self):
+        patient_ids = self.env['patient.list']
+        active_id = self.env.context.get('active_id')
+        patient_id = patient_ids.search([('id', '=', active_id)])
+        today = date.today()
+        patient_id.age = today.year - self.birthdate.year - (
+                    (today.month, today.day) < (self.birthdate.month, self.birthdate.day))
